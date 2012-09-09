00efc8612eca128ac80b8165c0bc853711466d48
TheB3Rt0z <web.bio.informatics@gmail.com>
2012-09-09 14:23:15 -0700

    added settings control module structure

---
+++ .app/control.php
@@ -0,0 +1,56 @@
+<?php
+
+class control extends controller
+{
+	function initialize()
+	{
+
+	}
+
+	protected function _handler()
+	{
+
+	}
+
+	private function _handler_core()
+	{
+
+	}
+
+	private function _handler_core_db()
+	{
+
+	}
+
+	private function _handler_core_controller()
+	{
+
+	}
+
+	private function _handler_core_navigator()
+	{
+
+	}
+
+	private function _handler_core_html()
+	{
+
+	}
+
+	private function _handler_core_widget()
+	{
+
+	}
+
+	private function _handler_core_language()
+	{
+
+	}
+
+	private function _handler_components()
+	{
+
+	}
+}
+
+?>
