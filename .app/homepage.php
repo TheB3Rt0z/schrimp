b39cd5720f665d936a03b303f86ef4c4e6ed7d47
TheB3Rt0z <matteo@192.168.0.30>
2012-10-17 13:31:44 +0200

    added user component (without binding) + corrections

--- .app/homepage.php
+++ .app/homepage.php
@@ -8,6 +8,18 @@ class homepage extends controller
 
         ob_start();
         ?>
+        <style>
+	        .notes
+			{
+			    width: 500px;
+			    padding: 10px;
+			    border-radius: 20px / 0 20px;
+			    background-color: darkgrey;
+			    box-shadow: 0px 0px 5px black;
+			    text-align: justify;
+			}
+        </style>
+
         <div class="notes">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris convallis pretium turpis, eget varius ipsum sodales quis. Nunc a dictum libero. Nulla quis mattis nulla. Pellentesque placerat, lorem et tristique rhoncus, neque nibh vulputate ipsum, nec dignissim nisi nisi nec orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce urna elit, faucibus non dapibus sed, pretium a ipsum. Etiam ac erat nec tellus vulputate congue a at mauris. Cras sem enim, feugiat at egestas et, viverra eu leo. Phasellus nec felis purus, vel ullamcorper augue. Sed nec urna magna, eu fermentum dui. Sed velit eros, lacinia venenatis consequat eget, dapibus sit amet eros. Aenean nibh velit, viverra vel fermentum at, aliquet sed massa. Sed eget mauris felis. Proin vestibulum aliquam ullamcorper.
 		</div>
