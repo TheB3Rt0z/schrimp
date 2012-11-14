72d0b1c8349e14c68acae2438bca9bb7ada69c91
Matteo Bertozzi <web.bio.informatics@gmail.com>
2012-11-14 23:52:42 +0100

    debug

--- .main.php
+++ .main.php
@@ -116,6 +116,7 @@ class main
     {
     	return md::title(1, PROJECT . " " . main::get_version())
     	     . md::image(".inc/img/schrimp_favicon.ico")
+    	     . md::image(".inc/img/schrimp.png")
     	     // adding real documentation with h2, h3, text and others..
     	     . str_repeat("\n", 5) . md::text(COPYRIGHT);
     }
